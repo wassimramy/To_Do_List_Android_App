5e965bb390d4d72c1c9ba54dd9c0b1af7e0a44ca
wrkhalil <wrkhalil@uark.edu>
2019-10-11 16:34:39 -0500

    Commenting the Model view classes

--- app/src/main/java/com/example/to_dolistapi25/Model/Notification.java
+++ app/src/main/java/com/example/to_dolistapi25/Model/Notification.java
@@ -35,35 +35,42 @@ public class Notification {
         this.requestCode = requestCode;
         this.context = context;
 
-        intent_0 = new Intent(context, Notification_0.class); //Create
-        pendingIntent_0 = PendingIntent.getBroadcast(context.getApplicationContext(), requestCode, intent_0, 0);
-        alarmManager_0 = (AlarmManager) context.getSystemService(ALARM_SERVICE);
-        intent_0.putExtra(Model.EXTRA_MESSAGE, itemID);
+        intent_0 = new Intent(context, Notification_0.class); //Create intent_0 for channel_0
+        pendingIntent_0 = PendingIntent.getBroadcast(context.getApplicationContext(), requestCode, intent_0, 0);  //Create pending_Intent_0, set id for the alarm and connect it with intent_0
+        alarmManager_0 = (AlarmManager) context.getSystemService(ALARM_SERVICE); //Create the alarmManager_0
 
-        intent_1 = new Intent(context, Notification_1.class);
-        pendingIntent_1 = PendingIntent.getBroadcast(context.getApplicationContext(), requestCode, intent_1, 0);
-        alarmManager_1 = (AlarmManager) context.getSystemService(ALARM_SERVICE);
+        intent_1 = new Intent(context, Notification_1.class); //Create intent_1 for channel_1
+        pendingIntent_1 = PendingIntent.getBroadcast(context.getApplicationContext(), requestCode, intent_1, 0); //Create pending_Intent_1, set id for the alarm and connect it with intent_1
+        alarmManager_1 = (AlarmManager) context.getSystemService(ALARM_SERVICE); //Create the alarmManager_1
 
-        intent_2 = new Intent(context, Notification_2.class);
-        pendingIntent_2 = PendingIntent.getBroadcast(context.getApplicationContext(), requestCode, intent_2, PendingIntent.FLAG_CANCEL_CURRENT);
-        alarmManager_2 = (AlarmManager) context.getSystemService(ALARM_SERVICE);
+        intent_2 = new Intent(context, Notification_2.class); //Create intent_2 for channel_2
+        pendingIntent_2 = PendingIntent.getBroadcast(context.getApplicationContext(), requestCode, intent_2, PendingIntent.FLAG_CANCEL_CURRENT); //Create pending_Intent_2, set id for the alarm and connect it with intent_2
+        alarmManager_2 = (AlarmManager) context.getSystemService(ALARM_SERVICE); //Create the alarmManager_2
 
     }
 
+    //Called to return today's date to set the alarms
      public static Date getTodaysDate (){
-        // get a calendar instance, which defaults to "now"
+        //Get a calendar instance, which defaults to "now"
         Calendar calendar = Calendar.getInstance();
+         //Return today's date
         return calendar.getTime();
     }
 
+    //Called to set notifications for an item
      void setAlarm() {
+
+         //Check whether the due date is after 24 hours
         if (notificationDateAndTime - getTodaysDate().getTime() >= 24*60*60*1000){
-            setNotification_0();
+            setNotification_0(); //If true, set a notification a day before the due
         }
+
+         //Check whether the due date is after an hour
         if (notificationDateAndTime - getTodaysDate().getTime() >= 60*60*1000){
-            setNotification_1();
+            setNotification_1(); //If true, set a notification an hour before the due
         }
-        setNotification_2();
+
+        setNotification_2(); //set a notification on the due date
     }
 
     private void setNotification_0() {
