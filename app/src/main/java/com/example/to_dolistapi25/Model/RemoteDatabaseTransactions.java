7c811ac1c1bea82596d3cbc4afdf25f21c0fc45a
wrkhalil <wrkhalil@uark.edu>
2019-10-11 16:57:11 -0500

    Commenting the Model view classes

--- app/src/main/java/com/example/to_dolistapi25/Model/RemoteDatabaseTransactions.java
+++ app/src/main/java/com/example/to_dolistapi25/Model/RemoteDatabaseTransactions.java
@@ -19,40 +19,44 @@ class RemoteDatabaseTransactions {
         }
     }
 
+    //Called to check the differences between remote and local databases
     private void checkDifferencesInRemoteDatabase(ItemDAO dao, List<Item> listRemoteDesktop, List<Item> list) {
-        deleteItemsFromRemoteDatabase (dao, listRemoteDesktop, list);
-        addOrEditAnItemInRemoteDatabase(dao, list);
+        deleteItemsFromRemoteDatabase (dao, listRemoteDesktop, list); //Delete the items from the remote database that does not exist in the local database
+        addOrEditAnItemInRemoteDatabase(dao, list); //Add or edit an item to the remote database
     }
 
+    //Called to check if an item exist in the remote database
     private void addOrEditAnItemInRemoteDatabase(ItemDAO dao, List<Item> list) {
         for (int i = 0 ; i < list.size(); i++){
             Item returnedItem = dao.findByID(list.get(i).iid);
             if (returnedItem == null){
-                dao.insertAll(list.get(i));
+                dao.insertAll(list.get(i)); //If not, insert the new item to the remote database
             }
             else {
                 dao.updateItem(list.get(i).iid, list.get(i).itemTitle, list.get(i).itemDescription,
-                        list.get(i).itemDateAndTime, list.get(i).itemStatus);
+                        list.get(i).itemDateAndTime, list.get(i).itemStatus); //If yes, update all the values of a given item
             }
         }
     }
 
+    //Called if there is no records in the database
     private void addItemsToRemoteDatabase(ItemDAO dao, List<Item> list) {
         for (int i = 0 ; i < list.size(); i++){
             dao.insertAll(list.get(i));
         }
     }
 
-    private void deleteItemsFromRemoteDatabase(ItemDAO dao,List<Item> listRemoteDesktop, List<Item> list) {
+    //Called to delete an item from the remote database if it is not present in the local database
+    private void deleteItemsFromRemoteDatabase(ItemDAO dao,List<Item> listRemoteDatabase, List<Item> list) {
         int found = 0;
-        for (int i = 0 ; i < listRemoteDesktop.size(); i++){
+        for (int i = 0 ; i < listRemoteDatabase.size(); i++){
             for (int j = 0 ; j < list.size(); j++){
-                if (listRemoteDesktop.get(i).iid == list.get(j).iid ){
+                if (listRemoteDatabase.get(i).iid == list.get(j).iid ){
                     found = 1;
                 }
             }
             if (found == 0){
-                dao.delete(listRemoteDesktop.get(i));
+                dao.delete(listRemoteDatabase.get(i));
             } else {
                 found = 0;
             }
